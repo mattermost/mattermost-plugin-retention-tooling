04039cde00c0691b46e6a4edda85fa0ae32c5ffc
wiggin77 <wiggin77@warpmail.net>
2023-09-25 18:53:58 -0400

    remove strings.CutPrefix usage

--- server/utils.go
+++ server/utils.go
@@ -20,3 +20,14 @@ func (p *Plugin) ensureSystemAdmin(userID string) error {
 
 	return nil
 }
+
+// CutPrefix returns s without the provided leading prefix string
+// and reports whether it found the prefix.
+// If s doesn't start with prefix, CutPrefix returns s, false.
+// If prefix is the empty string, CutPrefix returns s, true.
+func CutPrefix(s, prefix string) (after string, found bool) {
+	if !strings.HasPrefix(s, prefix) {
+		return s, false
+	}
+	return s[len(prefix):], true
+}
